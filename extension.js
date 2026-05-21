0c466b83fe8f46f8403ab0270292086504605c43
pcvargas <pcvargas@gmail.com>
2026-05-21 11:24:53 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,13 +1,33 @@
 ({
-    name: "LED RGB KidBright",
-    description: "Control de módulos LED RGB SMD 5050 usando exclusivamente los pines externos expuestos de la KidBright32.",
+    name: "LED RGB SMD",
+    description: "Control de módulos LED RGB SMD 2026 mediante canales PWM independientes en los GPIO 18, 19 y 23.",
     author: "Pablo Cesar Vargas Diaz",
     category: "Device Control",
     version: "1.0.0",
     icon: "/static/icon.png",
     color: "#8E44AD",
     blocks: [
-        "rgb_smd_setup", // Simplificado por el uso de Dropdowns
+        {
+            xml: `
+                <block type="rgb_smd_setup">
+                    <value name="pin_r">
+                        <shadow type="math_number">
+                            <field name="NUM">18</field>
+                        </shadow>
+                    </value>
+                    <value name="pin_g">
+                        <shadow type="math_number">
+                            <field name="NUM">19</field>
+                        </shadow>
+                    </value>
+                    <value name="pin_b">
+                        <shadow type="math_number">
+                            <field name="NUM">23</field>
+                        </shadow>
+                    </value>
+                </block>
+            `
+        },
         {
             xml: `
                 <block type="rgb_smd_set_color">
